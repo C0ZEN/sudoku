12c13aa46b5325622c082595ba67b315fdcacedb
C0ZEN <geoffrey.testelin@gmail.com>
2018-09-02 11:40:28 +0200

    fix: change regexp to bump version in jekyll config file

--- grunt/config/string-replace.js
+++ grunt/config/string-replace.js
@@ -23,7 +23,7 @@ module.exports = function () {
 			options: {
 				replacements: [
 					{
-						pattern    : /version: ([0-9]+<[.]){2}[0-9]+/g,
+						pattern    : /(version:)\s\d.\d.\d/gmi,
 						replacement: 'version: <%= newVersion %>'
 					}
 				]
