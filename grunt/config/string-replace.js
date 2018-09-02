460cfa29a2ffd11bed670cec68a8a73acc510ae7
C0ZEN <geoffrey.testelin@gmail.com>
2018-09-02 11:38:02 +0200

    build: add current date to changelog bump

--- grunt/config/string-replace.js
+++ grunt/config/string-replace.js
@@ -1,26 +1,37 @@
-module.exports = {
-	changelog    : {
-		src    : 'CHANGELOG.md',
-		dest   : 'CHANGELOG.md',
-		options: {
-			replacements: [
-				{
-					pattern    : /(\#\# \[Unreleased\])/g,
-					replacement: '## [Unreleased]\n\n## [<%= newVersion %>]'
-				}
-			]
+module.exports = function () {
+
+	const moment = require('moment');
+
+	moment.locale('en');
+
+	return {
+		changelog    : {
+			src    : 'CHANGELOG.md',
+			dest   : 'CHANGELOG.md',
+			options: {
+				replacements: [
+					{
+						pattern    : /(\#\# \[Unreleased\])/g,
+						replacement: '## [Unreleased]\n\n## [<%= newVersion %>] ' + getCurrentDate()
+					}
+				]
+			}
+		},
+		configVersion: {
+			src    : 'jekyll.yml',
+			dest   : 'jekyll.yml',
+			options: {
+				replacements: [
+					{
+						pattern    : /version: ([0-9]+<[.]){2}[0-9]+/g,
+						replacement: 'version: <%= newVersion %>'
+					}
+				]
+			}
 		}
-	},
-	configVersion: {
-		src    : 'jekyll.yml',
-		dest   : 'jekyll.yml',
-		options: {
-			replacements: [
-				{
-					pattern    : /version: ([0-9]+<[.]){2}[0-9]+/g,
-					replacement: 'version: <%= newVersion %>'
-				}
-			]
-		}
+	};
+
+	function getCurrentDate() {
+		return moment().format('YYYY-MM-DD HH:mm');
 	}
 };
